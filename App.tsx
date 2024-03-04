275e2ebac2b68f81e9cb75a91bf6203c42e2b864
Emil <emillau@nine-yi.com>
2024-03-04 21:41:30 +0800

    core: add tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}> 
-      <Text>hihi</Text>
+    <View className="bg-stone-800 w-full h-full"> 
+      <Text>fresh start</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
